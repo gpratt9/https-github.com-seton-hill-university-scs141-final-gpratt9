0196c17edf05f504087cf1151a838e803b76d093
Ravi Kiran HR <Ravi Kiran>
2022-04-02 22:39:57 +0530

    Initial Commit

---
+++ PizzaHut/PizzaHutApp.swift
@@ -0,0 +1,17 @@
+//
+//  PizzaHutApp.swift
+//  PizzaHut
+//
+//  Created by Ravi Kiran HR on 02/04/22.
+//
+
+import SwiftUI
+
+@main
+struct PizzaHutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
